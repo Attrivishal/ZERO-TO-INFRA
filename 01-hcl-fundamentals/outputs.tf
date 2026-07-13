3f736684d2978cbbff3ab49ff421ea8b8dfaf6a7
Vishal Attri <vishalattri196@gmail.com>
2026-07-13 23:32:43 +0530

    Explained How to write outpu.tf file.

--- 01-hcl-fundamentals/outputs.tf
+++ 01-hcl-fundamentals/outputs.tf
@@ -1,1 +1,6 @@
-#This file contains the outputs of the resources and modules we are going to use in orur terraform files.;
+#This file contains the outputs of the resources and modules we are going to use in our terraform files.
+
+output "instance_public_ip" {
+    description = "The public IP address of the EC2 Instance."
+    value       = aws_instance.web_server.public_ip
+}
