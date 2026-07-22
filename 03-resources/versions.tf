c2e9591cce01f6a60d0038733e1be3cd6369249e
Vishal Attri <vishalattri196@gmail.com>
2026-07-22 14:53:57 +0530

    New content added.

--- 03-resources/versions.tf
+++ 03-resources/versions.tf
@@ -5,7 +5,7 @@ terraform {
 
     required_providers {
         aws = {
-            source = "hashicorps/aws"
+            source = "hashicorp/aws"
             version = "~> 6.0"
         }
     }
