3ee6056f25aaac955ab3b04ca38945bb02e3c24e
Vishal Attri <vishalattri196@gmail.com>
2026-08-14 21:47:14 +0530

    New content added.

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -38,33 +38,34 @@ resource "aws_iam_policy" "developers_policy" {
 }
 
 # More policy for practice 
+# resource "aws_iam_policy" "developer_policy" {
+#   name        = var.policy_name
+#   description = "Policy for developer user"
 
-resource "aws_iam_policy" "developer_policy" {
-  name        = var.policy_name
-  description = "Policy for developer user"
+#   policy = jsonencode({
+#     Version = "2012-10-17"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
-        Resource = "arn:aws:s3:::company-bucket/*"
-      }
-    ]
-  })
-}
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "s3:GetObject",
+#           "s3:ListBucket"
+#         ]
+#         Resource = "arn:aws:s3:::company-bucket/*"
+#       }
+#     ]
+#   })
+# }
 
 #Membership of users in group
 # "[*] returns ALL users from a resource, so every group gets the same list of users—to assign different users, use a map that defines which users belong to which group."
 
 resource "aws_iam_group_membership" "developers_membership" {
   count = length(var.group_name)
-  name  = "developers-membership"
+
+  name  = "${aws_iam_group.developers[count.index].name}-membership"
+  # "${aws_iam_group.developers[count.index].name}-membership" we use this to create a unique name for each group membership resource based on the group name.
   group = aws_iam_group.developers[count.index].name
 
   users = aws_iam_user.developer[*].name
